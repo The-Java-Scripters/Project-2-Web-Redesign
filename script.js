b7b2ac1868ae5239b5cc06fa272860baa8cb902b
johnhdez105 <johnhdez105@gmail.com>
2021-12-07 22:53:40 -0600

    Add form field validation and dark mode tool

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
   $('.slider').bxSlider();
 });
+
+function validateForm() {
+var name = document.getElementById("fname").value;
+var email = document.getElementById("em").value;
+var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+if (name === '' || email === '') {
+  alert("Must fill in all fields");
+  return false;}
+  else {
+    return true;
+  }
+}
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
