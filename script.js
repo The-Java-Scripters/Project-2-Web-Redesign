f527691648967b9dc0e6de496bbbc8cd48240a93
johnhdez105 <johnhdez105@gmail.com>
2021-12-07 22:58:19 -0600

    fix validateform

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 function validateForm() {
 var name = document.getElementById("fname").value;
 var email = document.getElementById("em").value;
-var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
 if (name === '' || email === '') {
   alert("Must fill in all fields");
   return false;}
